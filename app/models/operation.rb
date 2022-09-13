d37b9448a1db545fdf68aeca9709b91b3a304090
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2022-09-13 10:12:05 +0900

    edit unit order

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,11 +2,11 @@ class Operation < ApplicationRecord
 
     def units
         ret = []
-        OpUnit.where(operation_id: self.id, is_enabled: true).order(:name).each do |ou|
+        OpUnit.where(operation_id: self.id, is_enabled: true).each do |ou|
             unit = Unit.find_by(id: ou.unit_id)
             ret.push(unit) unless unit.nil?
         end
-        return ret
+        return ret.sort_by{ |x| x.name }
     end
 
     def hash_format
